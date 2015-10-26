464aa0c63e06f34dc814984b76c4dea3a941ccea
Alessandrogit config --global user.name Alessandro <s55804@beuth-hochschule.de>
2015-10-26 12:41:29 +0100

    Client befehle und parameter-übergabe

--- src/uebung_1/Client.java
+++ src/uebung_1/Client.java
@@ -1,89 +1,112 @@
 package uebung_1;
 
-import javax.sound.midi.SysexMessage;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.Socket;
-import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 /**
- * Created by Mikel Reinhardt, Alessandro Furkim
- * on 05.10.2015.
+ * Created by Mikel Reinhardt, Alessandro Furkim on 05.10.2015.
  */
 public class Client {
 
-
     static Socket socket = null;
     static String string = "";
+    static String host;
+    static int port;
 
     /**
-     * Es wird ein Socket erzeugt mit den Portnummer <4444> und die schickt dann mit dem OutputStream
-     * die Zahl die berechnet werden soll
+     * Es wird ein Socket erzeugt mit den Portnummer <4444> und die schickt dann
+     * mit dem OutputStream die Zahl die berechnet werden soll
      */
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        if (args.length < 2) {
+            System.out.println("Fehler: fehlende Argumente.\n");
+            System.exit(0);
+        }
 
+        host = args[0];
+        port = Integer.parseInt(args[1]);
 
-        try{
-            socket = new Socket("localhost", 4444);
+        try {
+            socket = new Socket(host, port);
 
             PrintStream outputClient = new PrintStream(socket.getOutputStream(), true);
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            while(true){
-                eingabe();
+            while (true) {
+                Scanner s = new Scanner(System.in);
+                System.out.println("Geben Sie einen Befehl ein: ");
+                String cmd = s.next();
+                switch (cmd) {
+                    case "hilfe":
+                        System.out.println("Folgende Befehle sind : 'fibo (berechnet Fibonacci Zahl)'\n 'ende (Schließt die Verbindug zum Server)'");
+                        break;
+                    case "fibo":
+                        eingabe();
+                        outputClient.println(string);
 
-                outputClient.println(string);
+                        //Ausgabe der Antwort auf der Kommandozeile
+                        String serverResponse = null;
+                        while ((serverResponse = bufferedReader.readLine()) != null) {
 
-                //Ausgabe der Antwort auf der Kommandozeile
-                String serverResponse = null;
-                while ((serverResponse = bufferedReader.readLine()) != null) {
+                            System.out.println(serverResponse);
+                            eingabe();
+                        }
+                        break;
+                    case "ende":
+                        System.out.println("Socket closed");
+                        socket.close();
 
-                    System.out.println(serverResponse);
-                    eingabe();
                 }
+
             }
-        } catch (UnknownHostException e){
+        } catch (UnknownHostException e) {
             System.out.println("Unknown Host, please check the Host Input");
             e.printStackTrace();
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Input/Output Exception, please check your Input");
             e.printStackTrace();
         } finally {
-            if(socket != null){
-                try{
+            if (socket != null) {
+                try {
                     socket.close();
                     System.out.println("Socket closed");
-                } catch (IOException e){
+                } catch (IOException e) {
                     System.out.println("Socket problem, can't stop the socket");
                     e.printStackTrace();
                 }
             }
         }
     }
-    public static void eingabe(){
-        try{
-            while(isValide(string) == false){
+
+    public static void eingabe() {
+        try {
+            while (isValide(string) == false) {
                 System.out.println("Bitte geben sie eine Zahl ein.");
                 InputStreamReader inputStreamReader = new InputStreamReader(System.in);
                 BufferedReader reader = new BufferedReader(inputStreamReader);
                 string = reader.readLine();
             }
-        } catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static boolean isValide(String string){
-        if(string.equals("")){
+    public static boolean isValide(String string) {
+        if (string.equals("")) {
             return false;
         }
-        try{
+        try {
             int zahl = Integer.parseInt(string);
             System.out.println(zahl);
-            if(zahl > 0 && zahl < 100 ){
+            if (zahl > 0 && zahl < 100) {
                 return true;
             }
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             System.out.println("Ungueltige Eingabe: <" + string + "> ist keine Zahl.");
         }
         return false;
